55b026b2888f4451ec7393a1a26e399307ab66ca
NathanOD <nathan.odic@mines-ales.org>
2023-03-10 10:17:00 +0100

    added Departure and Arrival

--- API_Arrivee.py
+++ API_Arrivee.py
@@ -4,6 +4,7 @@ import json
 import datetime
 import os
 
+
 nb_trains = 100
 token = 'e7b7fedd-71d0-48c6-8cc7-749e22ba8e80'
 
@@ -12,7 +13,7 @@ def get_day(string):
     return string[6:8]+'-'+string[4:6]+'-'+string[0:4]
 
 def del_day(string):
-    return str(string)[9:]
+    return string[9:]
 
 def conv_min(string):
     return int(string[0:2])*60 + int(string[2:4])
@@ -29,10 +30,7 @@ def get_name(string):
     index_fin = string.find("', 'links'")
     return string[:index_fin]
 
-def get_id(string):
-    index_deb = string.find("'id': 'stop_point:")
-    string = string[index_deb+18:]
-    return string[:13]
+
 
 df_dic = pd.read_excel('select_gares.xlsx')
 dic_gare = df_dic.set_index('alias_libelle_noncontraint').T.to_dict('list')
@@ -41,15 +39,15 @@ df_final = pd.DataFrame()
 
 for gare in dic_gare:
     print(gare)
-    link = 'https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:' + dic_gare[gare][0] + '/departures?count=' + str(nb_trains)
+    link = 'https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:' + dic_gare[gare][0] + '/arrivals?count=' + str(nb_trains)
     req = requests.get(link,auth=(token, ''))
 
     doc = json.loads(req.text)
-    row = len(doc['departures'])
+    row = len(doc['arrivals'])
     #print(f'Nombre de trains : {row}')
 
     if row != 0:
-        df = pd.DataFrame(doc['departures'])
+        df = pd.DataFrame(doc['arrivals'])
         df_gare = pd.DataFrame(list(df['display_informations']))
         df_heure = pd.DataFrame(list(df['stop_date_time']))
         df_id = pd.DataFrame(list(df['links']))
@@ -69,13 +67,13 @@ for gare in dic_gare:
         df_gare = df_gare.drop(supr)
         df_heure = df_heure.drop(supr)
 
-        df_heure['departure_date_time'] = df_heure['departure_date_time'].apply(del_day)
-        df_heure['base_departure_date_time'] = df_heure['base_departure_date_time'].apply(del_day)
+        df_heure['arrival_date_time'] = df_heure['arrival_date_time'].apply(del_day)
+        df_heure['base_arrival_date_time'] = df_heure['base_arrival_date_time'].apply(del_day)
 
-        df_heure['retard'] = df_heure['departure_date_time'].apply(conv_min) - df_heure['base_departure_date_time'].apply(conv_min)
+        df_heure['retard'] = df_heure['arrival_date_time'].apply(conv_min) - df_heure['base_arrival_date_time'].apply(conv_min)
 
-        df_heure['heure'] = df_heure['departure_date_time'].apply(str_tps)
-        df_heure['old_heure'] = df_heure['base_departure_date_time'].apply(str_tps)
+        df_heure['heure'] = df_heure['arrival_date_time'].apply(str_tps)
+        df_heure['old_heure'] = df_heure['base_arrival_date_time'].apply(str_tps)
 
         df_gare['direction'] = df_gare['direction'].apply(del_par)
 
@@ -86,22 +84,21 @@ for gare in dic_gare:
             df_gare.rename(columns = {'trip_short_name':'Numéro'}, inplace = True)
 
             df_gare['Jour'] = df_heure['jour']
-            df_gare['Départ (réel)'] = df_heure['heure']
-            df_gare['Départ (prévu)'] = df_heure['old_heure']
+            df_gare['Arrivée (réelle)'] = df_heure['heure']
+            df_gare['Arrivée (prévue)'] = df_heure['old_heure']
             df_gare['Retard (min)'] = df_heure['retard']
             df_gare['id'] = df_id['id']
 
-        provenance = []
         arrets = []
         causes = []
-
+        provenance = []
         for index, row in df_gare.iterrows():
             id = row['id']
             
             if 'RealTime' in id:
                 index_id = id.index("RealTime")
                 id = id[:index_id-1]
-
+            
             link_voyage = 'https://api.sncf.com/v1/coverage/sncf/vehicle_journeys/' + id
             req_arret = requests.get(link_voyage ,auth=(token, ''))
             doc_voyage = json.loads(req_arret.text)
@@ -110,7 +107,6 @@ for gare in dic_gare:
             df_arret = pd.DataFrame(list(df_arret['stop_times']))
             df_arret = df_arret.T
             df_arret = pd.DataFrame(list(df_arret[0]))
-            df_arret['stop_point_id'] = df_arret['stop_point'].astype('str').apply(get_id)
             df_arret['stop_point'] = df_arret['stop_point'].astype('str').apply(get_name)
             provenance.append(df_arret['stop_point'][0])
 
@@ -124,24 +120,20 @@ for gare in dic_gare:
             else:
                 causes.append("")
 
-            liste_arrets = [list(df_arret['stop_point']),list(df_arret['stop_point_id'])]
-            index_gare = liste_arrets[1].index(dic_gare[gare][0])
-            
-            liste_arrets = liste_arrets[0][index_gare+1:]
+            liste_arrets = list(df_arret['stop_point'])
             arrets.append(liste_arrets)
-
 
         df_gare['Cause'] = causes
         df_gare['Arrêts'] = arrets
         df_gare.insert(0, 'Provenance', provenance)
         if len(df_gare) != 0:
             df_gare = df_gare.drop(['id'], axis=1)
-
+        
         print(f'Nombre de trains : {len(df_gare)}')
         if len(df_gare) != 0:
             df_final = pd.concat([df_final, df_gare], ignore_index=True)
 
-if os.path.exists('Departure.csv'):
-    os.remove('Departure.csv')
+if os.path.exists('Arrival.csv'):
+    os.remove('Arrival.csv')
 
-df_final.to_csv('Departure.csv', sep=',', index=False, header=True)
+df_final.to_csv('Arrival.csv', sep=',', index=False, header=True)
